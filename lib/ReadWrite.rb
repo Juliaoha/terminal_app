fe087bdb958ee163ee0030d5ffa91a3b730b6f6e
@Juliaoha <juliaoha@hotmail.com>
2022-04-23 10:05:43 +1000

    add ReadWrite

--- lib/ReadWrite.rb
+++ lib/ReadWrite.rb
@@ -1,2 +1,22 @@
+require 'json'
+
 class ReadWrite
+    def initialize(file_name)
+        @file_name = file_name
+    end
+
+    def write(term)
+        array = read
+        array.push(term)
+        File.open(@file_name, 'w') do |f|
+            f.write(JSON.pretty_generate(array))
+        end
+    end
+
+    def read
+        file = File.read(@file_name)
+        terms_array = JSON.parse(file)
+
+        return terms_array
+    end
 end
